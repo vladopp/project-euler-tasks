266ff73be392314ad91a8279cd5628792b808607
Vladimir Petkov <vlado@itgbg.com>
2017-07-15 17:45:45 +0300

    Simplifying solution.

--- 3-largest-prime-factor/src/net/projecteuler/Solution.java
+++ 3-largest-prime-factor/src/net/projecteuler/Solution.java
@@ -7,34 +7,16 @@ public class Solution {
 
 	public static void main(String[] args) {
 		long number = 600_851_475_143L;
-		long biggestPrimeFactor = 1;
-		long secondBiggestPrimeFactor = 1;
 		long currentDivisor = 3; // the number is not even so no need to divide by 2
 
 		while (number != 1) {
 			if (number % currentDivisor == 0) {
 				number /= currentDivisor;
-				secondBiggestPrimeFactor = biggestPrimeFactor;
-				biggestPrimeFactor = currentDivisor;
 			} else {
 				currentDivisor += 2;
 			}
 		}
-
-		if (isPrime(biggestPrimeFactor, secondBiggestPrimeFactor)) {
-			System.out.println(biggestPrimeFactor);
-		} else {
-			System.out.println(secondBiggestPrimeFactor);
-		}
+		
+		System.out.println(currentDivisor);
 	}
-
-	private static boolean isPrime(long number, long startingDivisor) {
-		for (long divisor = startingDivisor; divisor < number; divisor++) {
-			if (number % divisor == 0) {
-				return true;
-			}
-		}
-		return true;
-	}
-
 }
